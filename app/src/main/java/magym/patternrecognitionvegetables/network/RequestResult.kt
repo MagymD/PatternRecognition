d2b31ac926ac86d2dd573a7feb54b60aae0b97c4
Eduard <eduard.mishustin@yandex.ru>
2019-01-21 23:55:29 +0700

    Removing unnecessary files from the project..

--- app/src/main/java/magym/patternrecognitionvegetables/network/RequestResult.kt
+++
@@ -1,9 +0,0 @@
-package magym.patternrecognitionvegetables.network
-
-sealed class RequestResult<T> {
-
-    class Result<T>(val data: T) : RequestResult<T>()
-
-    class Error<T>(val error: Throwable) : RequestResult<T>()
-
-}
--- app/src/main/java/magym/patternrecognitionvegetables/utils/SnackbarExt.kt
+++
@@ -1,8 +0,0 @@
-package magym.patternrecognitionvegetables.utils
-
-import android.support.design.widget.Snackbar
-import android.view.View
-
-internal fun View.showSnackbarExt(text: String) {
-    Snackbar.make(this, text, Snackbar.LENGTH_LONG).show()
-}
